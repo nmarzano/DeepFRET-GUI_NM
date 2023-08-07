0dda6646a5102887d16953c058d9b1ce22fb39b9
nmarzano <80363768+nmarzano@users.noreply.github.com>
2023-08-07 16:54:42 +1000

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='DeepFRET-GUI_NM',
+    version='1.0.0',
+    url='https://github.com/nmarzano/DeepFRET-GUI_NM',
+    author='Nicholas Marzano',
+    author_email='nmarzano@uow.edu.au',
+    description='Package to be used for fitting FRET data with HMM model',
+    packages=find_packages(),    
+    # install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
